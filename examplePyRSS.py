605982af53d53c5ac7f0eca4907fa97b54007dcd
Winbobob <zhu6@ncsu.edu>
2015-02-10 17:10:38 -0500

    modify examplePyRSS.py

--- examplePyRSS.py
+++ examplePyRSS.py
@@ -7,29 +7,32 @@ rss = PyRSS2Gen.RSS2(
     title = "Car Info",
     link = "http://www.dalkescientific.com/Python/PyRSS2Gen.html",
     description = "the result of crawler",
-    
+
     mystrTime=time.strftime('%Y-%m-%d %X',time.localtime(time.time()))
     lastBuildDate = datetime.datetime.now(),
 
     items = [
        PyRSS2Gen.RSSItem(
-         title = "PyRSS2Gen-0.0 released",
-         link = "http://www.dalkescientific.com/news/030906-PyRSS2Gen.html",
-         description = "Dalke Scientific today announced PyRSS2Gen-0.0, "
-                       "a library for generating RSS feeds for Python.  ",
+         title = "model",
+         link = "",
+         description = "car model",
          guid = PyRSS2Gen.Guid("http://www.dalkescientific.com/news/"
                           "030906-PyRSS2Gen.html"),
-         pubDate = datetime.datetime(2003, 9, 6, 21, 31)),
+         pubDate = datetime.datetime(2015, 2, 10, 17, 00)),
        PyRSS2Gen.RSSItem(
-         title = "Thoughts on RSS feeds for bioinformatics",
-         link = "http://www.dalkescientific.com/writings/diary/"
-                "archive/2003/09/06/RSS.html",
-         description = "One of the reasons I wrote PyRSS2Gen was to "
-                       "experiment with RSS for data collection in "
-                       "bioinformatics.  Last year I came across...",
+         title = "year",
+         link = "",
+         description = "product year",
          guid = PyRSS2Gen.Guid("http://www.dalkescientific.com/writings/"
                                "diary/archive/2003/09/06/RSS.html"),
-         pubDate = datetime.datetime(2003, 9, 6, 21, 49)),
+         pubDate = datetime.datetime(2015, 2, 10, 17, 01)),
+       PyRSS2Gen.RSSItem(
+         title = "mile",
+         link = "",
+         description = "travel mile",
+         guid = PyRSS2Gen.Guid("http://www.dalkescientific.com/writings/"
+                               "diary/archive/2003/09/06/RSS.html"),
+         pubDate = datetime.datetime(2015, 2, 10, 17, 02)),
     ])
 
 rss.write_xml(open("car_info.xml", "w"))
