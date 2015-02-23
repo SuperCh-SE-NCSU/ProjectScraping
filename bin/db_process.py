133561c4cde4c19d3f41c6f2b11c5a4f7c285334
Winbobob <zhu6@ncsu.edu>
2015-02-23 13:32:59 -0500

    add readDB function

--- bin/db_process.py
+++ bin/db_process.py
@@ -23,4 +23,25 @@ def writeDB(username, make, model, year, email):
 	finally:
 		if con:
 		    con.close()
+		    
+def readDB():
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
+	con=None
+	try:
+		#local db
+		#con=psycopg2.connect(database='subscribe', user='administrator')
+		#Heroku remote db
+		con=psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+		cur=con.cursor()
+		cur.execute("SELECT * FROM users")
+		con.commit()
+		
+	except psycopg2.DatabaseError, e:
+		print 'Error %s' % e    
+		sys.exit(1)
+    
+	finally:
+		if con:
+		    con.close()
     
