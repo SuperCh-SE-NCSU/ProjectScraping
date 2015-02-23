bea6493a82f9a453e69f67951a37a15e8fb30591
Winbobob <zhu6@ncsu.edu>
2015-02-22 22:55:20 -0500

    modify db_process.py

--- bin/db_process.py
+++ bin/db_process.py
@@ -18,20 +18,3 @@ def writeDB(username, make, model, year, email):
 		if con:
 		    con.close()
     
-    
-def readDB():
-    con=None
-	try:
-		con=psycopg2.connect(database='subscribe', user='administrator')
-		cur=con.cursor()
-		cur.execute("SELECT * FROM users")
-		con.commit()
-		
-	except psycopg2.DatabaseError, e:
-		print 'Error %s' % e    
-		sys.exit(1)
-    
-	finally:
-		if con:
-		    con.close()
-    
