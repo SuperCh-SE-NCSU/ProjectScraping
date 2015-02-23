7a2cf74271031d94bf4eeeb5a65097a8a4529a22
Winbobob <zhu6@ncsu.edu>
2015-02-23 11:40:32 -0500

    connect to remote Heroku db

--- bin/db_process.py
+++ bin/db_process.py
@@ -1,11 +1,16 @@
 import psycopg2
 import sys
-
+import urlparse
 
 def writeDB(username, make, model, year, email):
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
 	con=None
 	try:
-		con=psycopg2.connect(database='subscribe', user='administrator')
+		#local db
+		#con=psycopg2.connect(database='subscribe', user='administrator')
+		#Heroku remote db
+		con=psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
 		cur=con.cursor()
 		cur.execute("INSERT INTO users VALUES ('" + username + "','" + make + "','" + model + "','" + year + "','" + email + "')")
 		con.commit()
