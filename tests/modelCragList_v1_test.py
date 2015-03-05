cd46201a6fa31ebf4362d99ff15daeb45115a3fb
Winbobob <zhu6@ncsu.edu>
2015-03-05 12:42:06 -0500

    fix test bugs:ImportError: No module

--- tests/modelCragList_v1_test.py
+++ tests/modelCragList_v1_test.py
@@ -1,5 +1,16 @@
-from modelCragList_v1_debug.py import *
+#add file(pythontemp_model) to PYTHONPATH in order to import being-tested file
+import os, sys
+path = os.path.abspath(os.path.join('pythontemp_model'))
+sys.path.append(path)
+
+print path
+print sys.path
+
+import modelCragList_v1_debug
 import unittest
 class Testcraigslist(unittest.TestCase):
     def setUp(self):
         self._init_()
+        
+if __name__ == '__main__':
+    unittest.main()
