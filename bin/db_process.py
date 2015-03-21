1fdd5d6f1d06028b947e3166b03bcbc42100f28b
Zhewei Hu <zhu6@ncsu.edu>
2015-03-20 21:55:57 -0400

    Update db_process.py

--- bin/db_process.py
+++ bin/db_process.py
@@ -8,13 +8,13 @@ import urlparse
 
 def writeDB(username, make, model, email, minYear, maxYear, minPrice, maxPrice, currentTime):
 	urlparse.uses_netloc.append("postgres")
-	#url = urlparse.urlparse(os.environ["DATABASE_URL"])
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
 	con=None
 	try:
 		#local db
-		con=psycopg2.connect(database='subscribe', user='administrator')
+		#con=psycopg2.connect(database='subscribe', user='administrator')
 		#Heroku remote db
-		#con=psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+		con=psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
 		cur=con.cursor()
 		cur.execute("INSERT INTO users VALUES ('" + username + "','" + make + "','" + model + "','" + email + "','" + minYear + "','" + maxYear + "','" + minPrice + "','" + maxPrice + "','" + currentTime + "')")
 		con.commit()
