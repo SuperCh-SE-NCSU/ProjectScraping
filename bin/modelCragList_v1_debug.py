096ea14811382ed946afee69faca5e2baedaae1f
Liang Dong <dragonfly90mad@gmail.com>
2015-03-23 10:50:47 -0400

    kbb

--- bin/modelCragList_v1_debug.py
+++ bin/modelCragList_v1_debug.py
@@ -108,5 +108,5 @@ def testcraglistsearch():
         print usercarlist.timepostlist[i]
         print '--------------------------'
         
-testcraglistsearch()
+#testcraglistsearch()
 #getMilageAndYear('http://raleigh.craigslist.org/cto/4902572544.html')
