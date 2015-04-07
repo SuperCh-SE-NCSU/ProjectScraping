953be9840234186618ef1297924381a1c98fcaf5
Winbobob <zhu6@ncsu.edu>
2015-04-07 11:56:05 -0400

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,57 @@
-import sys
-from setuptools.command.test import test as TestCommand
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
+def discover_and_run_tests():
+    import os
+    import sys
+    import unittest
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+    # get setup.py directory
+    setup_file = sys.modules['__main__'].__file__
+    setup_dir = os.path.abspath(os.path.dirname(setup_file))
 
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
+    # use the default shared TestLoader instance
+    test_loader = unittest.defaultTestLoader
 
+    # use the basic test runner that outputs to sys.stderr
+    test_runner = unittest.TextTestRunner()
+
+    # automatically discover all tests
+    # NOTE: only works for python 2.7 and later
+    test_suite = test_loader.discover(setup_dir)
+
+    # run the test suite
+    test_runner.run(test_suite)
+
+try:
+    from setuptools.command.test import test
+
+    class DiscoverTest(test):
+
+        def finalize_options(self):
+            test.finalize_options(self)
+            self.test_args = []
+            self.test_suite = True
+
+        def run_tests(self):
+            discover_and_run_tests()
+
+except ImportError:
+    from distutils.core import Command
+
+    class DiscoverTest(Command):
+        user_options = []
+
+        def initialize_options(self):
+                pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            discover_and_run_tests()
 
 setup(
     name='coveralls',
@@ -21,21 +59,17 @@ setup(
     packages=['coveralls'],
     url='http://github.com/coagulant/coveralls-python',
     license='MIT',
-    author='Ilya Baryshev',
-    author_email='baryshev@gmail.com',
-    description='Show coverage stats online via coveralls.io',
-    long_description=open('README.rst').read() + '\n\n' + open('CHANGELOG.rst').read(),
     entry_points={
         'console_scripts': [
             'coveralls = coveralls.cli:main',
         ],
     },
     install_requires=['docopt>=0.6.1', 'coverage>=3.6', 'requests>=1.0.0'],
-    tests_require=['mock', 'pytest', 'sh>=1.08'],
+    tests_require=['mock', 'unittest', 'sh>=1.08'],
     extras_require={
         'yaml': ['PyYAML>=3.10']
     },
-    cmdclass={'test': PyTest},
+    cmdclass={'test': DiscoverTest},
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Topic :: Software Development :: Testing',
@@ -43,11 +77,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
